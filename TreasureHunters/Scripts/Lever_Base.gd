1381f3f42100042fadab5a896287291d5cec41a9
Pope Scooby <PopeScooby@gmail.com>
2022-10-19 20:46:40 -0500

    tweaks

--- TreasureHunters/Scripts/Lever_Base.gd
+++ TreasureHunters/Scripts/Lever_Base.gd
@@ -10,11 +10,11 @@ func _ready():
 
 
 func _process(delta):
-	if Input.is_action_just_pressed("action_interact") and GlobalDictionaries.current_data["Flags"]["Near_LeverBase"] == true and GlobalDictionaries.current_data["Current_Item"] == "Handle" and GlobalDictionaries.current_data["Game_Info"]["Object_Interact"] == self.name and self.visible:
+	if Input.is_action_just_pressed("action_interact") and GlobalDictionaries.current_data["Flags"]["Near_LeverBase"] == true and GlobalDictionaries.current_data["Current_Item"] == "Handle" and GlobalDictionaries.current_data["Game_Info"]["Object_Interact"] == self.name and self.visible and GlobalDictionaries.current_data["Inventory"]["Handle"] > 0:
 		var Complete_Lever = get_node(Lever_Path)
 		Complete_Lever.visible = true
 		self.visible = false
-
+		GlobalDictionaries.current_data["Inventory"]["Handle"] -= 1
 
 func _on_Lever_Base_body_entered(body):
 	if body.name == "Adventurer" and self.visible:
